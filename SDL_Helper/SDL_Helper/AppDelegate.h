3c74e6bae904d631fe137046be022419d342b186
卢一鸣 <luyiming@weidian.com>
2017-02-23 17:11:12 +0800

    add webview and logview

--- SDL_Helper/SDL_Helper/AppDelegate.h
+++ SDL_Helper/SDL_Helper/AppDelegate.h
@@ -10,6 +10,7 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
-
+//menu
+-(IBAction)m_download:(id)sender;
 @end
 
--- SDL_Helper/SDL_Helper/AppDelegate.m
+++ SDL_Helper/SDL_Helper/AppDelegate.m
@@ -7,21 +7,43 @@
 //
 
 #import "AppDelegate.h"
+#import <WebKit/WebKit.h>
 
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet WebView *webview;
+@property (weak) IBOutlet NSTextView *logview;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    
+    //load config file
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+
+-(IBAction)m_download:(id)sender{
+    [self appendToMyTextView:@"download menu clic"];
+    //NSLog(@"download menu click");
+}
+
+
+- (void)appendToMyTextView:(NSString*)text
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSAttributedString* attr = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@\n", text]];
+        [[self.logview textStorage] appendAttributedString:attr];
+        [self.logview scrollRangeToVisible:NSMakeRange([[self.logview string] length], 0)];
+    });
 }
 
 
